8611c1f183553c7ed2087dc5a3383a94b89b8b35
Bradley Wong <bkwong1990@gmail.com>
2019-12-23 20:05:08 -0800

    The laser is OP

---
+++ my_sprites.py
@@ -0,0 +1,230 @@
+#Change list
+#12/23/2019: Initial checkin
+
+import pygame
+from pygame.locals import (
+RLEACCEL,
+K_UP,
+K_DOWN,
+K_LEFT,
+K_RIGHT
+)
+#RNG needed for enemies and clouds
+import random
+
+#A class representing the player character
+class Player(pygame.sprite.Sprite):
+    '''
+    Creates a new player
+    Parameters:
+        self: the object being created
+        left_bound, right_bound, top_bound, bottom_bound (int): the numbers indicating the boundaries of the space the player is in
+        player_speed (int): The amount of pixels the player can move per frame. Diagonal movement is faster
+        color_key ( (int, int, int) ): The color to be considered transparent for the converted sprite image. RGB numbers.
+    '''
+    def __init__(self, left_bound, right_bound, top_bound, bottom_bound, player_speed, colorkey):
+        super(Player, self).__init__()
+        #Set image
+        self.surf = pygame.image.load("jet5.png").convert()
+        self.surf.set_colorkey( colorkey, RLEACCEL )
+        self.rect = self.surf.get_rect()
+        self.left_bound = left_bound
+        self.right_bound = right_bound
+        self.top_bound = top_bound
+        self.bottom_bound = bottom_bound
+        self.player_speed = player_speed
+
+    '''
+    Updates the player's position based on the pressed keys. The player cannot pass the boundaries of the containing area.
+    Parameters:
+        self: The calling object
+        pressed_keys: a collection that shows which keys were pressed for the current frame
+    '''
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -self.player_speed)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, self.player_speed)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-self.player_speed, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(self.player_speed, 0)
+
+        #Check if in bounds
+        self.rect.left = self.left_bound if (self.rect.left < self.left_bound) else self.rect.left
+        self.rect.right = self.right_bound if (self.rect.right > self.right_bound) else self.rect.right
+        self.rect.top = self.top_bound if (self.rect.top < self.top_bound) else self.rect.top
+        self.rect.bottom = self.bottom_bound if (self.rect.bottom > self.bottom_bound) else self.rect.bottom
+
+#A missile to shoot down the player
+class Missile(pygame.sprite.Sprite):
+    '''
+    Creates a new missile
+    Parameters:
+        self: the object being created
+        left_bound, right_bound, top_bound, bottom_bound (int): the numbers indicating the boundaries of the space the missile is in
+        missile_maxspeed (int): The maximum speed the missile can get from the RNG
+        color_key ( (int, int, int) ): The color to be considered transparent for the converted sprite image. RGB numbers.
+    '''
+    def __init__(self, left_bound, right_bound, top_bound, bottom_bound, missile_maxspeed, colorkey):
+        super(Missile, self).__init__()
+        self.surf = pygame.image.load("missile5.png").convert()
+        self.surf.set_colorkey( colorkey, RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                #Missile can generate out of view on right side of screen
+                random.randint(right_bound + 20, right_bound + 100),
+                random.randint(0, bottom_bound)
+            )
+        )
+        self.left_bound = left_bound
+        self.speed = random.randint(5, missile_maxspeed)
+    '''
+    Updates the missile's position. The missile will be killed upon going through the left side of the containing space.
+    Parameters:
+        self: The calling object
+    '''
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < self.left_bound:
+            self.kill()
+#A cloud that does nothing
+class Cloud(pygame.sprite.Sprite):
+    '''
+    Creates a new cloud
+    Parameters:
+        self: the object being created
+        left_bound, right_bound, top_bound, bottom_bound (int): the numbers indicating the boundaries of the space the cloud is in
+        cloud_speed (int): The cloud's speed
+        color_key ( (int, int, int) ): The color to be considered transparent for the converted sprite image. RGB numbers.
+    '''
+    def __init__(self, left_bound, right_bound, top_bound, bottom_bound, cloud_speed, colorkey):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("cloud3.png").convert()
+        self.surf.set_colorkey( colorkey, RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(right_bound + 20, right_bound + 100),
+                random.randint(top_bound, bottom_bound)
+            )
+        )
+        self.left_bound = left_bound
+        self.speed = cloud_speed
+
+    '''
+    Updates the cloud's position. The cloud will be killed upon going through the left side of the containing space.
+    Parameters:
+        self: The calling object
+    '''
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < self.left_bound:
+            self.kill()
+
+#A tank enemy that follows the player's position and shoots lasers
+class LaserTank(pygame.sprite.Sprite):
+    LASER_COOLDOWN = 300
+    SPAWN_TIME = 10000
+    '''
+    Creates a new tank
+    Parameters:
+        self: the object being created
+        left_bound, right_bound, bottom_bound (int): the numbers indicating the boundaries of the space the tank is in
+        tank_speed (int): The number of pixels the tank can move per frame
+        color_key ( (int, int, int) ): The color to be considered transparent for the converted sprite image. RGB numbers.
+        target (anything with a Rect instance variable): The object being tracked by the tank
+        laser_event_type (event type): The event type to be used to create a laser
+        respawn_event_type (event type): The event type to be used to force another tank to spawn
+    '''
+    def __init__(self, left_bound, right_bound, bottom_bound, tank_speed, colorkey, target, laser_event_type, respawn_event_type):
+        super(LaserTank, self).__init__()
+        self.surf = pygame.image.load("tank2.png").convert()
+        self.surf.set_colorkey(colorkey, RLEACCEL)
+
+        width = self.surf.get_rect().width
+        height = self.surf.get_rect().height
+        self.rect = self.surf.get_rect()
+
+        self.rect.centerx = random.randint(left_bound + width // 2, right_bound - width // 2)
+        self.rect.bottom = bottom_bound
+
+        self.left_bound = left_bound
+        self.right_bound = right_bound
+        self.bottom_bound = bottom_bound
+
+        self.target = target
+        self.speed = tank_speed
+        self.event_on_death = respawn_event_type
+        self.frames_until_laser = LaserTank.LASER_COOLDOWN
+        self.laser_event_type = laser_event_type
+        self.frames_until_movement = 0;
+
+    '''
+    Updates the tank's position. The tank will track the player, but cannot pass the boundaries of the screen.
+    Parameters:
+        self: The calling object
+    '''
+    def update(self):
+        if self.frames_until_movement <= 0:
+            if self.target.rect.centerx < self.rect.centerx:
+                self.rect.move_ip(-self.speed, 0)
+            elif self.target.rect.centerx > self.rect.centerx:
+                self.rect.move_ip(self.speed, 0)
+
+            self.rect.left = self.left_bound if (self.rect.left < self.left_bound) else self.rect.left
+            self.rect.right = self.right_bound if (self.rect.right > self.right_bound) else self.rect.right
+        else:
+            #The tank can't move until it's done firing the laser.
+            self.frames_until_movement = self.frames_until_movement - 1
+
+        #The tank has a cooldown time for the laser
+        if self.frames_until_laser <= 0:
+            self.frames_until_laser = LaserTank.LASER_COOLDOWN
+            self.frames_until_movement = Laser.LASER_DURATION
+            self.rect.bottom = self.bottom_bound
+            #creates a laser event object that also contains the coordinates to start the laser at
+            laser_event = pygame.event.Event(self.laser_event_type, centerx = self.rect.centerx, bottom = self.rect.top)
+            pygame.event.post(laser_event)
+        else:
+            if self.frames_until_laser <= 60:
+                #Suspend movement for one second before the laser fires.
+                self.frames_until_movement = Laser.LASER_DURATION
+            self.frames_until_laser = self.frames_until_laser - 1
+    '''
+    Kills the tank, but not before setting a one-time timer to force another tank to spawn
+    '''
+    def kill(self):
+        pygame.time.set_timer(respawn_event_type, LaserTank.SPAWN_TIME, True)
+        pygame.sprite.Sprite.kill(self)
+
+#A laser that pierces the heavens
+class Laser(pygame.sprite.Sprite):
+    LASER_DURATION = 10
+    '''
+    Creates a new laser
+    Parameters:
+        self: The object being created
+        centerx: The horizontal coordinate of the laser's center
+        bottom: The vertical coordinate of the laser's bottom
+        color_key ( (int, int, int) ): The color to be considered transparent for the converted sprite image. RGB numbers.
+    '''
+    def __init__(self, centerx, bottom, colorkey):
+        super(Laser, self).__init__()
+        self.surf = pygame.image.load("laser.png").convert()
+        self.surf.set_colorkey(colorkey, RLEACCEL)
+
+        self.rect = self.surf.get_rect()
+        self.rect.centerx = centerx
+        self.rect.bottom = bottom
+
+        self.frame_duration = Laser.LASER_DURATION
+
+    '''
+    Updates the laser's position and counts down the frames until the laser expires
+    Parameters:
+        self: The calling object
+    '''
+    def update(self):
+        if self.frame_duration == 0:
+            self.kill()
+        self.frame_duration = self.frame_duration - 1
---
+++ noCreativity.py
@@ -0,0 +1,195 @@
+'''
+Todo:
+Enemy tanks. Drop bombs on them. Tanks can shoot back.
+Have plane shoot missiles
+'''
+
+import pygame
+
+#RNG
+#import random
+
+from pygame.locals import (
+RLEACCEL,
+K_UP,
+K_DOWN,
+K_LEFT,
+K_RIGHT,
+K_ESCAPE,
+K_z,
+K_x,
+KEYDOWN,
+QUIT,
+)
+
+import my_sprites
+
+#Screen Resolution
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+#Battle Surface dimensions
+BATTLE_WIDTH = 1000
+BATTLE_HEIGHT = SCREEN_HEIGHT
+
+BATTLE_X = SCREEN_WIDTH - BATTLE_WIDTH
+
+HARD_SPEED_MAX = 50
+HARD_SPEED_MIN = 10
+
+SKY_COLOR = (135, 206, 235)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+missile_maxspeed = 20
+player_speed = 7
+cloud_speed = 2
+tank_speed = 1
+
+#initialize score
+score = 0
+
+pygame.init()
+
+#Create screen object
+
+screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+
+#create battle Surface
+battle_surf = pygame.Surface( (BATTLE_WIDTH, BATTLE_HEIGHT) )
+battle_surf.fill( SKY_COLOR )
+
+#create info Surface
+info_surface = pygame.Surface( (BATTLE_X, SCREEN_HEIGHT) )
+info_surface.fill( WHITE )
+
+#create font for info area
+font_size = 28
+info_font = pygame.font.Font(None, font_size)
+
+#Create a custom event for adding a new Enemy
+ADDMISSILE = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDMISSILE, 250)
+
+#Custom event and timer for clouds
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
+
+#Custom event for Tanks Laser
+ADDTANK = pygame.USEREVENT + 3
+ADDLASER = pygame.USEREVENT + 4
+
+#instantiate Player
+player = my_sprites.Player(BATTLE_X, SCREEN_WIDTH, 0, BATTLE_HEIGHT, player_speed, SKY_COLOR)
+
+#create a group of enemies
+enemies = pygame.sprite.Group()
+pygame.time.set_timer(ADDTANK, 250, True)
+
+#create a group of clouds
+clouds = pygame.sprite.Group()
+#create a group of all game pieces (enemies + player)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Setup clock to change framerate
+clock = pygame.time.Clock()
+
+#define functions
+def handle_events(events):
+    global missile_maxspeed
+    global score
+    running = True
+    for event in events:
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+            elif event.key == K_x:
+                missile_maxspeed += 10
+                missile_maxspeed = min(missile_maxspeed, HARD_SPEED_MAX)
+            elif event.key == K_z:
+                missile_maxspeed -= 10
+                missile_maxspeed = max(missile_maxspeed, HARD_SPEED_MIN)
+                score = score // 2
+        elif event.type == QUIT:
+            running = False
+        elif event.type == ADDMISSILE:
+            #create a new enemy and add it to sprite groups
+            new_missile = my_sprites.Missile(BATTLE_X, SCREEN_WIDTH, 0, BATTLE_HEIGHT, missile_maxspeed, SKY_COLOR)
+            enemies.add(new_missile)
+            all_sprites.add(new_missile)
+        elif event.type == ADDCLOUD:
+            #create new cloud
+            new_cloud = my_sprites.Cloud(BATTLE_X, SCREEN_WIDTH, 0, BATTLE_HEIGHT, cloud_speed, SKY_COLOR)
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
+        elif event.type == ADDTANK:
+            new_tank = my_sprites.LaserTank(BATTLE_X, SCREEN_WIDTH, BATTLE_HEIGHT, tank_speed, SKY_COLOR, player, ADDLASER, ADDTANK)
+            enemies.add(new_tank)
+            all_sprites.add(new_tank)
+        elif event.type == ADDLASER:
+            new_laser = my_sprites.Laser(event.centerx, event.bottom, SKY_COLOR)
+            enemies.add(new_laser)
+            all_sprites.add(new_laser)
+    return running
+
+def write_info():
+    screen.blit(info_surface, (0, 0) )
+    #display score
+    score_text = info_font.render("Score: %010d" % score, True, BLACK )
+    screen.blit(score_text, (5, 5))
+    #displayspeed
+    speed_text = info_font.render("Speed: %03d" % missile_maxspeed, True, BLACK )
+    screen.blit(speed_text, (5, font_size * 2))
+
+    #Display instructions
+
+    lower_speed_text = info_font.render("Slow down w/Z (1/2 Score)", True, BLACK )
+    screen.blit(lower_speed_text, (5, font_size * 4))
+
+    raise_speed_text = info_font.render("Speed up w/X", True, BLACK )
+    screen.blit(raise_speed_text, (5, font_size * 6))
+
+    #Display FPS
+    fps_text = info_font.render("FPS: %.2f" % clock.get_fps(), True, BLACK )
+    screen.blit(fps_text, (5, font_size * 8))
+
+running = True
+while running:
+    #Go through event collection
+    running = handle_events(pygame.event.get())
+
+    #Get the key press list and update the player's position
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    #update enemy position
+    enemies.update()
+    clouds.update()
+
+    #fill screen with white
+    screen.fill( WHITE )
+
+    #draw battle portion of screen
+    screen.blit(battle_surf, (BATTLE_X, 0) )
+
+    #draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    #draw info Surface
+    write_info()
+
+    #Check if any enemies have collided with the Player
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        #if a collision occured, remove the player and stop the loop
+        player.kill()
+        running = False
+
+    score += missile_maxspeed
+    pygame.display.flip()
+
+    #Force fps
+    clock.tick(60)
+print("Final score: %010d" % score)
